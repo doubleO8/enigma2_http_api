86f04ca2b06d3637ec271fdf439b9dc622ba5791
doubleO8 <wb008@hdm-stuttgart.de>
2017-08-17 21:06:55 +0200

    display eit value, verbosity CLI arg, display delete timer URL

--- eha-timer-list.py
+++ eha-timer-list.py
@@ -6,7 +6,7 @@ import os
 import argparse
 import datetime
 import pprint
-
+import urllib
 import pytz
 
 from enigma2_http_api.controller import Enigma2APIController
@@ -31,28 +31,51 @@ class TimerLister(Enigma2APIController):
         dt_keys = ['begin', 'end', 'startprepare', 'realbegin', 'realend']
         for item in timerlist:
             for key in dt_keys:
+                tkey = '{:s}_dt'.format(key)
                 try:
                     dt_obj = datetime.datetime.fromtimestamp(item[key])
                     if dt_obj.tzinfo is None:
                         dt_obj = LOCALTIMEZONE.localize(dt_obj)
-                    item[key] = dt_obj
+                    item[tkey] = dt_obj
                 except TypeError:
                     pass
 
-            print '{servicename:50s} {begin} -- {end}'.format(
+            try:
+                eit = int(item.get("eit", -1))
+            except Exception:
+                eit = 0
+
+            print '#{eit:06d} {servicename:50s} {begin_dt} -- {end_dt}'.format(
                 servicename=item['servicename'],
-                begin=item['begin'].strftime('%d.%m.%Y %H:%M'),
-                end=item['end'].strftime('%H:%M')
+                begin_dt=item['begin_dt'].strftime('%d.%m.%Y %H:%M'),
+                end_dt=item['end_dt'].strftime('%H:%M'),
+                eit=eit
             )
             print u'{name}'.format(name=item['name'])
-            print u'{description:50s} ({duration}s) {pseudo_id}'.format(
+            print u'{description:50s} ({duration}s) PSEUDO_ID={pseudo_id}'.format(
                 description=item['description'],
                 duration=item['duration'],
                 pseudo_id=pseudo_unique_id(item)
             )
             print " "
-            print u'{descriptionextended}'.format(**item)
-            # pprint.pprint(item)
+
+            if self.args.verbose > 0:
+                print u'{descriptionextended}'.format(**item)
+
+            if self.args.verbose > 1:
+                query_params = {
+                    "sRef": item.get("serviceref"),
+                    "begin": item.get("begin"),
+                    "end": item.get("end"),
+                }
+                del_url = 'http://{remote_addr}/api/timerdelete?{query}'.format(
+                    remote_addr=self.args.remote_addr,
+                    query=urllib.urlencode(query_params))
+                print("delete URL: {:s}".format(del_url))
+
+            if self.args.verbose > 2:
+                pprint.pprint(item)
+
             print " "
 
 
@@ -68,7 +91,8 @@ if __name__ == '__main__':
     argparser.add_argument('--timezone', dest="local_timezone",
                            default='Europe/Berlin',
                            help="local timezone, default %(default)s")
-
+    argparser.add_argument('-v', '--verbose',
+                            action='count', default=0, dest='verbose')
     args = argparser.parse_args()
 
     tli = TimerLister(remote_addr=args.remote_addr,
